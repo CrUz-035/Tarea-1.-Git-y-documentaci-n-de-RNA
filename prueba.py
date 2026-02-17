8024c38d831a938e5a20ab9ed366ee488b7ecab7
Francisco Romero Mestiza <115271821+CrUz-035@users.noreply.github.com>
2026-02-16 21:43:56 -0600

    Comentarios para prueba.py
    Esta parte del código se obtuvo del archivo "test.py" para el código en "network.py".
    Las fotos relacionadas a la ejecución del archivo "network.py" y este archivo se encuentran en la carpeta "Fotos evidencia".

--- prueba.py
+++ prueba.py
@@ -1,9 +1,16 @@
-import mnist_loader
+#Esta parte del código se obtuvo del archivo "test.py" para el código en "network.py"
+#las fotos relacionadas a la ejecución del archivo "network.py" y este archivo se 
+#encuentran en la carpeta "Fotos evidencia".
+
+import mnist_loader #Se cargan los datos de mnist.
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-training_data = list(training_data)
+#load_data_wrapper() segmenta el conjunto de imagenes en conjuntos para entrenamiento,
+#validación y prueba.
+training_data = list(training_data) #Se convierten los datos entrenados a una lista.
 
-import network
+import network # Se importa la red neuronal creada anteriormente.
 
 
-net = network.Network([784, 30, 10])
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+net = network.Network([784, 30, 10]) #Esto es, la red tiene 784 neuronas de entrada,
+#30 neuronas ocultas y 10 de salida, relacionandose con los digitos del 0 al 9.
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)#Se entrena la red.
